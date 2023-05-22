126941e668270cf2334dde9a6e3f003225ac01d3
Hyunfor <navel5148@gmail.com>
2023-05-22 11:04:32 +0900

    ProductListServlet 생성 및 코딩

---
+++ src/com/mis/controller/ProductListServlet.java
@@ -0,0 +1,57 @@
+package com.mis.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mis.dao.ProductDAO;
+import com.mis.dto.ProductVO;
+
+/**
+ * Servlet implementation class ProductListServlet
+ */
+@WebServlet("/productList.do")
+public class ProductListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 데이터베이스에서 상품목록 가져오기
+		ProductDAO pDao = ProductDAO.getInstance();
+		List<ProductVO> productList = pDao.selectAllProducts();
+		
+		// request 속성에 상품 목록 정보 담기
+		request.setAttribute("productList", productList);
+		
+		// 목록페이지로 이동
+		RequestDispatcher dispatcher = request.getRequestDispatcher("product/productList.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
